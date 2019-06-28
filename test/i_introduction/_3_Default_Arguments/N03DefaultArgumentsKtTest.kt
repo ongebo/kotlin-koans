afed7c011def10eaa18da6edf98afb722f84d94b
ongebo <isaacongebo@gmail.com>
2019-06-28 12:10:15 +0300

    Implement foo() in JavaCode3.java using default arguments

--- test/i_introduction/_3_Default_Arguments/N03DefaultArgumentsKtTest.kt
+++ test/i_introduction/_3_Default_Arguments/N03DefaultArgumentsKtTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class N03DefaultArgumentsKtTest {
-
     @Test
     fun testDefaultAndNamedParams() {
         assertEquals("a42b1C42D2", task3())
