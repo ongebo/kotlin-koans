8ffe3e027701b49b693147d5026fd0ef78365d1e
ongebo <isaacongebo@gmail.com>
2019-06-28 11:52:10 +0300

    Solve TODO task 2

--- test/i_introduction/_2_Named_Arguments/N02NamedArgumentsKtTest.kt
+++ test/i_introduction/_2_Named_Arguments/N02NamedArgumentsKtTest.kt
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class N02NamedArgumentsKtTest {
-
     @Test
     fun testJoinToString() {
         assertEquals("{1, 2, 3, 42, 555}", task2(listOf(1, 2, 3, 42, 555)))
     }
-
 }
